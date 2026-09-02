5f3de03fc96c836ca7182c0f1bb6a52eb3910fc2
bzabk <bzabkowski@gmail.com>
2026-09-02 15:49:23 +0200

    registration for athena executor

---
+++ src/pipeline/registered_executors.py
@@ -0,0 +1,9 @@
+from pipeline.infrastructure_registration import InfrastructureRegistration, register_new_infrastructure
+
+register_new_infrastructure(
+    InfrastructureRegistration(
+        name="Athena",
+        executor_path="pipeline.executor_adapters.athena_adapter.AthenaExecutor",
+        completion_path="pipeline.executor_adapters.athena_adapter.AthenaCompletionSource",
+    )
+)
